838391a69087770f83e9e5dc7ff3760b4893df74
Michael Granger <ged@FaerieMUD.org>
2016-02-18 12:04:39 -0800

    Use content-selectors instead of replaceable templates for element-section

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -7,30 +7,31 @@ var bump = require('gulp-bump');
 var args = require('../args');
 
 gulp.task('bump-version', function(){
-  return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump({type:args.bump })) //major|minor|patch|prerelease
-    .pipe(gulp.dest('./'));
+	return gulp.src(['./package.json', './bower.json'])
+	.pipe(bump({type:args.bump })) //major|minor|patch|prerelease
+	.pipe(gulp.dest('./'));
 });
 
 gulp.task('changelog', function(callback) {
-  var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+	var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-  return changelog({
-    repository: pkg.repository.url,
-    version: pkg.version,
-    file: paths.doc + '/CHANGELOG.md'
-  }, function(err, log) {
-    fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
-  });
+	return changelog({
+		repository: pkg.repository.url,
+		version: pkg.version,
+		file: paths.doc + '/CHANGELOG.md'
+	}, function(err, log) {
+		fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
+	});
 });
 
 gulp.task('prepare-release', function(callback){
-  return runSequence(
-    'build',
-    'lint',
-    'bump-version',
-    'doc',
-    'changelog',
-    callback
-  );
+	return runSequence(
+		'build',
+		'lint',
+		'bump-version',
+		'doc',
+		'changelog',
+		callback
+	);
 });
+
