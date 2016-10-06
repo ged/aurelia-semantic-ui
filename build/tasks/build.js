d073a50a7ec46835da0b7b5ab54fc35249795011
Michael Granger <ged@FaerieMUD.org>
2016-10-06 16:44:26 -0700

    Sync up with the latest build stuff from the plugin skeleton

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,64 +1,69 @@
-var gulp = require('gulp');
-var debug = require('gulp-debug');
-var runSequence = require('run-sequence');
-var to5 = require('gulp-babel');
-var sourcemaps = require('gulp-sourcemaps');
-var paths = require('../paths');
+let gulp = require('gulp');
+let runSequence = require('run-sequence');
+let to5 = require('gulp-babel');
+let paths = require('../paths');
+let compilerOptions = require('../babel-options');
+let assign = Object.assign || require('object.assign');
+let debug = require('gulp-debug');
 
-gulp.task('build-html-es6', function () {
-	return gulp.src(paths.html).
-		pipe(gulp.dest(paths.output + 'es6'));
+gulp.task('build-html', function() {
+	return gulp.src( paths.html ).
+		pipe( debug({ title: 'build-html' }) ).
+		pipe( gulp.dest(paths.output + 'es2015') ).
+		pipe( gulp.dest(paths.output + 'commonjs') ).
+		pipe( gulp.dest(paths.output + 'amd') ).
+		pipe( gulp.dest(paths.output + 'system') );
 });
 
-gulp.task('build-css-es6', function () {
-	return gulp.src(paths.css).
-		pipe(gulp.dest(paths.output + 'es6'));
+gulp.task('build-css', function() {
+	return gulp.src( paths.css ).
+		pipe( debug({ title: 'build-css' }) ).
+		pipe( gulp.dest(paths.output + 'es2015') ).
+		pipe( gulp.dest(paths.output + 'commonjs') ).
+		pipe( gulp.dest(paths.output + 'amd') ).
+		pipe( gulp.dest(paths.output + 'system') );
 });
 
-gulp.task('build-es6', function () {
-	return gulp.src(paths.source).
-		pipe(debug({title: 'build-es6'})).
-		pipe(gulp.dest(paths.output + 'es6'));
+gulp.task('build-es2015', function() {
+	return gulp.src( paths.source ).
+		pipe( debug({ title: 'build-es2015' }) ).
+		pipe( to5(assign({}, compilerOptions.es2015())) ).
+		pipe( gulp.dest(paths.output + 'es2015') );
 });
 
-gulp.task('build-html-amd', function () {
-	return gulp.src(paths.html).
-		pipe(gulp.dest(paths.output + 'amd'));
+gulp.task('build-commonjs', function() {
+	return gulp.src( paths.source ).
+		pipe( debug({ title: 'build-commonjs' }) ).
+		pipe( to5(assign({}, compilerOptions.commonjs())) ).
+		pipe( gulp.dest(paths.output + 'commonjs') );
 });
 
-gulp.task('build-css-amd', function () {
-	return gulp.src(paths.css).
-		pipe(gulp.dest(paths.output + 'amd'));
+gulp.task('build-amd', function() {
+	return gulp.src( paths.source ).
+		pipe( debug({ title: 'build-amd' }) ).
+		pipe( to5(assign({}, compilerOptions.amd())) ).
+		pipe( gulp.dest(paths.output + 'amd') );
 });
 
-gulp.task('build-amd', function () {
-	return gulp.src(paths.source).
-		pipe(debug({title: 'build-amd'})).
-		pipe(sourcemaps.init()).
-		pipe(to5()).
-		pipe(sourcemaps.write()).
-		pipe(gulp.dest(paths.output + 'amd'));
+gulp.task('build-system', function() {
+	return gulp.src( paths.source ).
+		pipe( debug({ title: 'build-system' }) ).
+		pipe( to5(assign({}, compilerOptions.system())) ).
+		pipe( gulp.dest(paths.output + 'system') );
 });
-
-gulp.task('build-html', [
-	'build-html-es6',
-	'build-html-amd'
-]);
-
-gulp.task('build-css', [
-	'build-css-es6',
-	'build-css-amd'
-]);
-
-gulp.task('build-js', [
-	'build-es6',
-	'build-amd'
-]);
 
 gulp.task('build', function(callback) {
 	return runSequence(
 		'clean',
-		['build-js', 'build-html', 'build-css'],
+		[
+			'build-html',
+			'build-css',
+			// 'build-es2015',
+			// 'build-commonjs',
+			'build-amd',
+			'build-system'
+		],
 		callback
 	);
 });
+
