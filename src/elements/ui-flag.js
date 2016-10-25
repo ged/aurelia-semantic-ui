d352720b38a086c9c673b0285b5f8023f0851414
Michael Granger <ged@FaerieMUD.org>
2016-10-25 14:53:51 -0700

    Add ui-flag implementation

--- src/elements/ui-flag.js
+++ src/elements/ui-flag.js
@@ -2,8 +2,8 @@
 "use strict";
 
 /**
- * Flag - http://semantic-ui.com/elements/flag.html
- */
+* Flag - http://semantic-ui.com/elements/flag.html
+*/
 
 import {constants} from '../constants';
 import {uiElement, uiAttribute} from '../decorators';
@@ -11,26 +11,29 @@ import {SemanticUIElement, SemanticUIAttribute} from '../ui-base';
 import {bindable} from 'aurelia-framework';
 
 @uiElement( 'flag' )
-export class SemanticUIFlagElement extends SemanticUIElement {}
+export class SemanticUIFlagElement extends SemanticUIElement {
+	@bindable country;
+}
+
 
 @uiAttribute( 'flag' )
 export class SemanticUIFlagAttribute extends SemanticUIAttribute {
 
-    @bindable country;
+	@bindable country;
 
 
-    bind( ...args ) {
-        super.bind( ...args );
-        if ( this.country ) {
-            this.element.classList.add( ...this.country.split(' ') );
-        }
-    }
+	bind( ...args ) {
+		// No super, as we don't need the 'ui' class
+		this.addCssClasses( 'flag' );
+		if ( this.country ) {
+			this.addCssClasses( this.country.toLowerCase() );
+		}
+	}
 
-
-    countryChanged( newValue, oldValue ) {
-        if ( oldValue ) { this.element.classList.remove(...oldValue.split(' ')); }
-        if ( newValue ) { this.element.classList.add(...newValue.split(' ')); }
-    }
+	countryChanged( newValue, oldValue ) {
+		if ( oldValue ) { this.removeCssClasses(oldValue.toLowerCase()); }
+		if ( newValue ) { this.addCssClasses(newValue.toLowerCase()); }
+	}
 
 }
 
---
+++ test/elements/ui-flag_spec.js
@@ -0,0 +1,120 @@
+/* -*- javascript -*- */
+"use strict";
+
+import {StageComponent} from 'aurelia-testing';
+import {bootstrap} from 'aurelia-bootstrapper';
+import {LogManager} from 'aurelia-framework';
+import {ConsoleAppender} from 'aurelia-logging-console';
+import {customMatchers} from '../helpers';
+
+
+describe('ui-flag', () => {
+	let component, logger;
+
+	beforeAll(() => {
+		jasmine.addMatchers( customMatchers );
+		logger = LogManager.getLogger( 'ui-flag-spec');
+	});
+
+	beforeEach(() => {
+		component = StageComponent.withResources('src/elements/ui-flag');
+	});
+
+	afterEach(() => {
+		try {
+			component.dispose();
+		} catch(e) {
+			logger.debug( "Error disposing the StageComponent" );
+		}
+	});
+
+
+	describe( 'as a custom element', () => {
+
+		it( 'adds the country as a class', done => {
+			component.
+				inView(`
+					<ui-flag country="Ghana"></ui-flag>
+				`).
+				boundTo({}).
+				create( bootstrap ).then( () => {
+					let flag = component.element.firstElementChild;
+					expect( flag ).toBeHtmlTag( 'i' );
+					expect( flag ).toHaveCssClasses( 'ghana', 'flag' );
+					expect( flag ).not.toHaveCssClasses( 'ui' );
+				}).
+				then( done ).
+				catch( done.fail );
+		});
+
+
+		it( 'can get the country from a bound value', done => {
+			component.
+				inView(`
+					<ui-flag country.bind="address.country"></ui-flag>
+				`).
+				boundTo({
+					address: {
+						line1: '101 Independence Ave SE',
+						city: 'Washington',
+						state:'DC',
+						postal_code: '20540',
+						country: 'us'
+					}
+				}).
+				create( bootstrap ).then( () => {
+					let flag = component.element.firstElementChild;
+					expect( flag ).toBeHtmlTag( 'i' );
+					expect( flag ).toHaveCssClasses( 'us', 'flag' );
+					expect( flag ).not.toHaveCssClasses( 'ui' );
+				}).
+				then( done ).
+				catch( done.fail );
+		});
+
+	});
+
+
+	describe( 'as a custom attribute', () => {
+
+		it( 'adds the country as a class', done => {
+			component.
+				inView(`
+					<i ui-flag="country: ae"></i>
+				`).
+				boundTo({}).
+				create( bootstrap ).then( () => {
+					expect( component.element ).toHaveCssClasses( 'ae', 'flag' );
+					expect( component.element ).not.toHaveCssClasses( 'ui' );
+				}).
+				then( done ).
+				catch( done.fail );
+		});
+
+
+		it( 'can get the country from a bound value', done => {
+			component.
+				inView(`
+					<i ui-flag="country.bind: address.country"></i>
+				`).
+				boundTo({
+					address: {
+						line1: 'Saarstraße 21',
+						postal_code: '55122',
+						city: 'Mainz',
+						country: 'Germany'
+					}
+				}).
+				create( bootstrap ).then( () => {
+					expect( component.element ).toHaveCssClasses( 'germany', 'flag' );
+					expect( component.element ).not.toHaveCssClasses( 'ui' );
+				}).
+				then( done ).
+				catch( done.fail );
+		});
+
+
+	});
+
+
+});
