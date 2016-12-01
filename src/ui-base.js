fe2e2c64491c29965e5ce20824f19395962218c6
Michael Granger <ged@FaerieMUD.org>
2016-12-01 15:17:15 -0800

    Make UIAttributes use their UIElement if they’re on one, and filter ‘true’ and ‘false’ class strings.

--- src/ui-base.js
+++ src/ui-base.js
@@ -22,8 +22,8 @@ function splitIntoClasses( name ) {
  * 'au-' to avoid messing with Aurelia's classes.
  * @return {Array[String]} the flattened list of CSS classes.
  */
-export function expandCssClasses( ...cssClasses ) {
-	let normalized = cssClasses.
+function expandCssClasses( ...cssClasses ) {
+	let expanded = cssClasses.
 		map( (cl) => {
 			if ( Array.isArray(cl) ) {
 				return expandCssClasses( ...cl );
@@ -31,9 +31,25 @@ export function expandCssClasses( ...cssClasses ) {
 				return splitIntoClasses( cl );
 			}
 		});
-	let flattened = [].concat.apply( [], normalized );
+	let flattened = [].concat.apply( [], expanded );
 
 	return flattened;
+}
+
+
+/**
+ * Expand the classes in {cssClasses} with expandCssClasses() and then filter out 
+ * non-classes and Aurelia-internal classes.
+ */
+function normalizeCssClasses( ...cssClasses ) {
+	let classList = expandCssClasses( ...cssClasses );
+	return classList.
+		filter( (cl) => {
+			return cl !== '' &&
+				cl !== 'true' &&
+				cl !== 'false' &&
+				!cl.startsWith( 'au-' )
+		});
 }
 
 
@@ -71,8 +87,7 @@ export class SemanticUIElement {
 	 * Add one or more {cssClasses} to the Semantic UI element.
 	 */
 	addCssClasses( ...cssClasses ) {
-		let expandedClasses = expandCssClasses( cssClasses ).
-			filter( (cl) => cl !== '' && !cl.startsWith('au-') );
+		let expandedClasses = normalizeCssClasses( cssClasses );
 		if ( expandedClasses.length ) {
 			this.semanticElement.classList.add( ...expandedClasses );
 		}
@@ -83,7 +98,7 @@ export class SemanticUIElement {
 	 * Remove one or more {cssClasses} from the Semantic UI element.
 	 */
 	removeCssClasses( ...cssClasses ) {
-		let expandedClasses = expandCssClasses( cssClasses ).
+		let expandedClasses = normalizeCssClasses( cssClasses ).
 			filter( (cl) => cl !== '' && !cl.startsWith('au-') );
 		if ( expandedClasses.length ) {
 			this.semanticElement.classList.remove( ...expandedClasses );
@@ -97,12 +112,16 @@ export class SemanticUIElement {
 	 * @return {Element} the element to add CSS classes to.
 	 */
 	getSemanticElement( parentEl ) {
+		this.logger.debug( "Fetching inner semantic element for parent element: ", parentEl );
+
 		// Containerless elements get passed a comment anchor element
 		if ( parentEl.nodeType === 8 ) {
+			this.logger.debug( "  Containerless element; using previous sibling." );
 			return parentEl.previousElementSibling;
 		}
 
 		else if ( parentEl.nodeType === 1 ) {
+			this.logger.debug( "  HTML element; using first element child: ", parentEl.firstElementChild );
 			return parentEl.firstElementChild;
 		}
 
@@ -154,10 +173,10 @@ export class SemanticUIAttribute {
 
 	/**
 	 * Add one or more {cssClasses} to the Semantic UI element. The values in {cssClasses}
-	 * will be expanded by expandCssClasses().
+	 * will be expanded by normalizeCssClasses().
 	 */
 	addCssClasses( ...cssClasses ) {
-		let expandedClasses = expandCssClasses( cssClasses );
+		let expandedClasses = normalizeCssClasses( cssClasses );
 		this.semanticElement.classList.add( ...expandedClasses );
 	}
 
@@ -166,7 +185,7 @@ export class SemanticUIAttribute {
 	 * Remove one or more {cssClasses} from the Semantic UI element.
 	 */
 	removeCssClasses( ...cssClasses ) {
-		let expandedClasses = expandCssClasses( cssClasses );
+		let expandedClasses = normalizeCssClasses( cssClasses );
 		this.semanticElement.classList.remove( ...expandedClasses );
 	}
 
@@ -177,7 +196,25 @@ export class SemanticUIAttribute {
 	 * @return {Element} the element to add CSS classes to.
 	 */
 	getSemanticElement( el ) {
-		return el;
+		// If this is an attribute of a Semantic UI element, use its semantic element.
+		if (
+			el.au &&
+			el.au.controller &&
+			el.au.controller.viewModel &&
+			el.au.controller.viewModel instanceof SemanticUIElement
+		) {
+			let container = el.au.controller.viewModel;
+			if ( container instanceof SemanticUIElement ) {
+				this.logger.debug( "Decorating another Semantic element: ", container );
+				let semEl = container.getSemanticElement( el );
+				this.logger.debug( "Other element's semantic element is: ", semEl );
+				return semEl;
+			}
+			return el;
+		} else {
+			this.logger.debug( "Decorating a plain HTML element: ", el );
+			return el;
+		}
 	}
 
 }
