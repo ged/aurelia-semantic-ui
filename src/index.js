be7946f0d7a72f57270e7140f439565a94de31ca
Michael Granger <ged@FaerieMUD.org>
2017-03-03 16:47:20 -0800

    Bump the prerelease version

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import jQuery from 'jquery';
 import 'semantic-ui';
 
 
-export var VERSION = '0.0.1-alpha.18';
+export var VERSION = '0.0.1-alpha.19';
 
 export * from './behaviors/ui-visibility';
 
